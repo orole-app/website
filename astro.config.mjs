81f2cddc31c8cb13305163ce9c4317272e8c95ff
Ray Orolé <orole.ray@protonmail.com>
2023-10-04 12:34:10 +0200

    Remove internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,56 +4,66 @@ import sitemap from "@astrojs/sitemap";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 import rehypeSlug from "rehype-slug";
-import astroI18next from "astro-i18next";
 import alpinejs from "@astrojs/alpinejs";
 import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://orole.studio",
-  vite: {
-    define: {
-      __DATE__: `'${new Date().toISOString()}'`
-    }
-  },
-  integrations: [tailwind(), sitemap(), astroI18next(), alpinejs(), AstroPWA({
-    mode: "development",
-    base: "/",
-    scope: "/",
-    includeAssets: ["favicon.svg"],
-    registerType: "autoUpdate",
-    manifest: {
-      name: "Orole - Your web development agency",
-      short_name: "Orole",
-      theme_color: "#ffffff",
-      icons: [{
-        src: "pwa-192x192.png",
-        sizes: "192x192",
-        type: "image/png"
-      }, {
-        src: "pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png"
-      }, {
-        src: "pwa-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable"
-      }]
-    },
-    workbox: {
-      navigateFallback: "/404",
-      globPatterns: ["*.js"]
-    },
-    devOptions: {
-      enabled: true,
-      navigateFallbackAllowlist: [/^\/404$/],
-      suppressWarnings: true
-    }
-  })],
-  markdown: {
-    rehypePlugins: [rehypeSlug,
-    // This adds links to headings
-    [rehypeAutolinkHeadings, autolinkConfig]]
-  }
+	site: "https://orole.studio",
+	vite: {
+		define: {
+			__DATE__: `'${new Date().toISOString()}'`,
+		},
+	},
+	integrations: [
+		tailwind(),
+		sitemap(),
+		alpinejs(),
+		AstroPWA({
+			mode: "development",
+			base: "/",
+			scope: "/",
+			includeAssets: ["favicon.svg"],
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Orole - Your web development agency",
+				short_name: "Orole",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+			workbox: {
+				navigateFallback: "/404",
+				globPatterns: ["*.js"],
+			},
+			devOptions: {
+				enabled: true,
+				navigateFallbackAllowlist: [/^\/404$/],
+				suppressWarnings: true,
+			},
+		}),
+	],
+	markdown: {
+		rehypePlugins: [
+			rehypeSlug,
+			// This adds links to headings
+			[rehypeAutolinkHeadings, autolinkConfig],
+		],
+	},
 });
